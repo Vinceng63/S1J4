3fa8c970b65bf45332e68c3c3eedb416e6f014e6
lucaspiquet <48794360+lucaspiquet@users.noreply.github.com>
2019-04-05 01:04:16 +0200

    Modif finale des pyramides : hauteur / 2

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -6,20 +6,30 @@ def top_pyramide(steps)
 end
 
 def bottom_pyramide(steps)
-	# todo 
+    nb_briques = steps * 2 - 3
+    for i in (1..steps-1)
+      puts (" " * i) + ("#"*(nb_briques))
+      nb_briques -= 2
+    end
 end
 
 def losange(steps)
-	top_pyramide(steps)
-	bottom_pyramide(steps)
+    top_pyramide(steps / 2 + 1)
+    bottom_pyramide(steps / 2 + 1)
 end
 
 def perform 
-	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu entre 1 et 25?"
-	print "> "
-	user_choice = gets.to_i
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu entre 1 et 25?"
+    print "> "
+    user_choice = gets.to_i
+    
+    while user_choice % 2 == 0
+        puts "Un nombre impair je t'ai dit!!!"
+        print "> "
+        user_choice = gets.to_i
+    end
 
-	losange(user_choice)
+    losange(user_choice)
 end
 
 perform
